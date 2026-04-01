64cbd1e2fef4e734989ddf937219384f1964ae90
TanishKumari <144725361+TanishKumari@users.noreply.github.com>
2026-04-01 23:45:32 +0530

    Update ShopkeeperRegistration.jsx

--- frontend/src/pages/ShopkeeperRegistration.jsx
+++ frontend/src/pages/ShopkeeperRegistration.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import './Shopregisteration.css';
+import './RegistrationPages.css';
 
 const ShopkeeperRegistration = () => {
 
